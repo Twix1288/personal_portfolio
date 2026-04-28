323f31b7abcc76ad6712fd0b28edbdff0fedb2eb
Twix1288 <rishitagni@gmail.com>
2026-04-27 19:06:12 -0700

    Fix settings module reference

--- core/wsgi.py
+++ core/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_port.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
